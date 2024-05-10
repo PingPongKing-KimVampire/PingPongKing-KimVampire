57a465de006da3a1e2663dabe6cf11761f6b5c62
lru0409 <lru040409@gmail.com>
2024-05-10 16:07:02 +0900

    feat: 공, 패들 위치를 % 단위로 설정

--- soloGameStory/app.js
+++ soloGameStory/app.js
@@ -15,8 +15,6 @@ exitYesButton.addEventListener('click', () => {
 	console.log('exit!');
 })
 
-console.log("으아아앙");
-
 // 패들 움직이기
 
 const leftPlayBoard = document.querySelector('.subPlayBoard:nth-of-type(1)');
@@ -29,21 +27,21 @@ document.body.addEventListener('mousemove', (e) => {
 	const paddleWidth = paddle.element.clientWidth;
 	const boardHeight = leftPlayBoard.clientHeight;
 	const boardWidth = leftPlayBoard.clientWidth;
-
-	// leftPlayBoard의 뷰포트 위치 받기
-	const rect = leftPlayBoard.getBoundingClientRect();
-
+	
 	// 마우스 위치를 기준으로 패들의 새로운 위치 구하기
-	paddle.y = e.clientY - rect.top - paddleHeight / 2;
-	paddle.x = e.clientX - rect.left - paddleWidth / 2;
+	const leftBoardRect = leftPlayBoard.getBoundingClientRect();
+	paddle.y = ((e.clientY - leftBoardRect.top - (paddleHeight / 2)) / boardHeight) * 100;
+	paddle.x = ((e.clientX - leftBoardRect.left - (paddleWidth / 2)) / boardWidth) * 100;
 
 	// 패들이 보드를 벗어나지 않도록 위치 조정하기
-	paddle.y = Math.max(0, Math.min(paddle.y, boardHeight - paddleHeight));
-	paddle.x = Math.max(0, Math.min(paddle.x, boardWidth - paddleWidth));
+	const maxY = ((boardHeight - paddleHeight) / boardHeight) * 100;
+	const maxX = ((boardWidth - paddleWidth) / boardWidth) * 100;
+	paddle.y = Math.max(0, Math.min(paddle.y, maxY));
+	paddle.x = Math.max(0, Math.min(paddle.x, maxX));
 
 	// 실제 패들의 위치를 변경하기
-	paddle.element.style.top = `${paddle.y}px`;
-	paddle.element.style.left = `${paddle.x}px`;
+	paddle.element.style.top = `${paddle.y}%`;
+	paddle.element.style.left = `${paddle.x}%`;
 })
 
 
@@ -56,45 +54,61 @@ function calculateMovement(distance, angle) {
 	return { dx, dy };
 }
 
-function initBall(ball) {
-	ball.cx = playBoard.clientWidth/2 - ball.element.clientWidth/2;
-	ball.cy = playBoard.clientHeight/2 - ball.element.clientWidth/2;
-	ball.angle = 45;
-	ball.speed = 3;
-	const movement = calculateMovement(ball.speed, ball.angle);
-	ball.dx = movement.dx;
-	ball.dy = movement.dy;
-	ball.element.style.top = `${ball.cy}px`;
-	ball.element.style.left = `${ball.cx}px`;
-}
-
-let isMyTurn = false;
 const playBoard = document.querySelector('#playBoard');
 const ball = {
 	element: document.querySelector('#playBoard #ball'),
-	angle: 45,
-	speed: 3
+	init() {
+		const radiusX = ((this.element.clientWidth / 2) / playBoard.clientWidth) * 100;
+		const radiusY = ((this.element.clientHeight / 2) / playBoard.clientHeight) * 100;
+		this.cx = 50 - radiusX;
+		this.cy = 50 - radiusY;
+		this.angle = 30;
+		this.speed = 0.3;
+		const movement = calculateMovement(this.speed, this.angle);
+		this.dx = movement.dx;
+		this.dy = movement.dy;
+		this.element.style.left = `${this.cx}%`;
+		this.element.style.top = `${this.cy}%`;
+	},
+	changeDx() {
+		let rand = Math.floor(Math.random() * 21) - 10; // -10 ~ 10도 사이에서 이동 방향 변화
+		ball.angle = Math.max(0, Math.min(45, ball.angle + rand)); // 최소 각도 0, 최대 각도 45
+		const movement = calculateMovement(ball.speed, ball.angle);
+		ball.dx = ball.dx < 0 ? movement.dx : -movement.dx; // dx는 부호 반전
+		ball.dy = ball.dy < 0 ? -movement.dy : movement.dy; // dy는 부호 유지
+	}
 };
-initBall(ball);
+
+let isPlaying = false;
+let isMyTurn = false;
+let ballMoveIntervalID;
+
+function startGame(ball) {
+	isPlaying = true;
+	isMyTurn = false;
+	ballMoveIntervalID = setInterval(moveBall, 1);
+}
+
+function stopGame(ball) {
+	isPlaying = false;
+	clearInterval(ballMoveIntervalID);
+	ball.init();
+}
 
 function detectWall() {
-	const boardH = playBoard.clientHeight;
-	const boardW = playBoard.clientWidth;
-	const ballH = ball.element.clientHeight;
-	const ballW = ball.element.clientWidth;
+	const ballRect = ball.element.getBoundingClientRect();
+	const boardRect = playBoard.getBoundingClientRect();
 
 	// 공이 왼쪽 벽과 만나면 초기 위치로 리셋
-	if (ball.cx <= 0) {
-		isPlaying = false;
-		clearInterval(ballMoveIntervalID);
-		initBall(ball);
+	if (ballRect.left <= boardRect.left) {
+		stopGame(ball);
 		return;
 	}
-	// 공이 위, 아래, 오른쪽 벽과 만나면 이동 방향 전환
-	if (ball.cy < 0 || boardH - ballH < ball.cy) {
+	// 공이 위, 아래쪽 벽과 만나면 이동 방향 전환
+	if (ballRect.top <= boardRect.top || boardRect.bottom <= ballRect.bottom) {
 		ball.dy = -ball.dy;
 	}
-	if (boardW - ballW < ball.cx) {
+	if (boardRect.right <= ballRect.right) {
 		isMyTurn = true; // 공이 오른쪽 벽과 만나면 턴 전환
 		ball.dx = -ball.dx;
 	}
@@ -109,7 +123,7 @@ function detectPaddle() {
 		ballRect.bottom > paddleRect.top &&
 		ballRect.top < paddleRect.bottom
 	) {
-		ball.dx = -ball.dx;
+		ball.changeDx();
 		isMyTurn = false;
 	}
 }
@@ -117,23 +131,19 @@ function detectPaddle() {
 function moveBall() {
 	ball.cy += ball.dy;
 	ball.cx += ball.dx;
-	ball.element.style.top = `${ball.cy}px`;
-	ball.element.style.left = `${ball.cx}px`;
+	ball.element.style.top = `${ball.cy}%`;
+	ball.element.style.left = `${ball.cx}%`;
 	detectWall();
 	if (isMyTurn)
 		detectPaddle();
 }
 
-let isPlaying = false;
-let ballMoveIntervalID;
+ball.init();
 
 document.body.addEventListener('mousedown', () => {
 	if (isPlaying) {
-		isPlaying = false;
-		clearInterval(ballMoveIntervalID);
-		initBall(ball);
+		stopGame(ball);
 	} else {
-		isPlaying = true;
-		ballMoveIntervalID = setInterval(moveBall, 1);
+		startGame(ball);
 	}
 })
