6999ae2da843d8bf115b67284db772b6537e0328
RoWoon Lee <lru040409@gmail.com>
2024-06-14 13:33:03 +0900

    feat: 대기실 생성 페이지 반응형 고려하여 수정
    - HTML 구조 변경
    - CSS 클래스 정리
    - CSS 코드 정리 + 반응형 코드 추가

--- fe/js/LobbyPage/WaitingRoomCreationPageManager.js
+++ fe/js/LobbyPage/WaitingRoomCreationPageManager.js
@@ -32,13 +32,13 @@ class WaitingRoomCreationPageManager {
 		let isSelectedHumanCount = false;
 
 		if (isSelectedMode) {
-			if (selectedModeButton.value === "vampireVsHuman") {
-				this.countSelection.classList.replace('invisibleSelectionGroup', 'visibleSelectionGroup');
-				this.modeSelection.style.marginBottom = '8%';
+			if (selectedModeButton.value === 'vampireVsHuman') {
+				this.countSelection.classList.replace('invisible', 'visible');
+				this.modeSelection.classList.add('selectionGroupBottomMargin');
 				isSelectedHumanCount = this.humanCountButtons.find((button) => button.checked) !== undefined;
 			} else {
-				this.countSelection.classList.replace('visibleSelectionGroup', 'invisibleSelectionGroup');
-				this.modeSelection.style.marginBottom = '0%';
+				this.countSelection.classList.replace('visible', 'invisible');
+				this.modeSelection.classList.remove('selectionGroupBottomMargin');
 				isSelectedHumanCount = true;
 			}
 		}
@@ -55,60 +55,72 @@ class WaitingRoomCreationPageManager {
 
 	_getHTML() {
 		return `
-			<button class="exitButton"></button>
-			<div id="roomSettingContainer">
-				<div class="selectionGroup">${this._getTitleSelectionHTML()}</div>
-				<div class="selectionGroup">${this._getModeSelectionHTML()}</div>
-				<div class="selectionGroup invisibleSelectionGroup">
-					${this._getPlayerCountSelectionHTML()}
+			<div id="container">
+				<button class="exitButton"></button>
+				<div id="roomSettingContainer">
+					<div class="selectionGroup selectionGroupBottomMargin">
+						${this._getTitleSelectionHTML()}
+					</div>
+					<div class="selectionGroup">
+						${this._getModeSelectionHTML()}
+					</div>
+					<div class="selectionGroup invisible">
+						${this._getPlayerCountSelectionHTML()}
+					</div>
 				</div>
+				<button id="completeButton" class="disabledButton">방 생성하기</button>
 			</div>
-			<button id="completeButton" class="disabledButton">방 생성하기</button>
 		`;
 	}
 
 	_getTitleSelectionHTML() {
 		return `
 			<label class="selectionLabel" for="titleInput">방 제목</label>
-			<input type="text" id="titleInput">
+			<div class="selectionBox">
+				<input type="text" id="titleInput">
+			</div>
 		`;
 	}
 
 	_getModeSelectionHTML() {
 		return `
 			<label class="selectionLabel">모드</label>
-			<input type="radio" name="mode" id="humanVsHuman" value="humanVsHuman">
-			<label for=humanVsHuman class="modeButton">인간 VS 인간</label>
-			<input type="radio" name="mode" id="vampireVsVampire" value="vampireVsVampire">
-			<label for=vampireVsVampire class="modeButton">뱀파이어 VS 뱀파이어</label>
-			<input type="radio" name="mode" id="vampireVsHuman" value="vampireVsHuman">
-			<label for=vampireVsHuman class="modeButton">뱀파이어 VS 인간</label>
+			<div class="selectionBox">
+				<input type="radio" name="mode" id="humanVsHuman" value="humanVsHuman">
+				<label for=humanVsHuman class="modeButton">인간 VS 인간</label>
+				<input type="radio" name="mode" id="vampireVsVampire" value="vampireVsVampire">
+				<label for=vampireVsVampire class="modeButton">뱀파이어 VS 뱀파이어</label>
+				<input type="radio" name="mode" id="vampireVsHuman" value="vampireVsHuman">
+				<label for=vampireVsHuman class="modeButton">뱀파이어 VS 인간</label>
+			</div>
 		`;
 	}
 
 	_getPlayerCountSelectionHTML() {
 		return `
 			<label class="selectionLabel">인간</label>
-			<div class="countButtonGroup" id="humanCountButtonGroup">
-				<input type="radio" name="humanCount" id="human2" , value="2">
-				<label for="human2" class="countButton">2</label>
-				<input type="radio" name="humanCount" id="human3" , value="3">
-				<label for="human3" class="countButton">3</label>
-				<input type="radio" name="humanCount" id="human4" , value="4">
-				<label for="human4" class="countButton">4</label>
-				<input type="radio" name="humanCount" id="human5" , value="5">
-				<label for="human5" class="countButton">5</label>
-				<input type="radio" name="humanCount" id="human6" , value="6">
-				<label for="human6" class="countButton">6</label>
+			<div class="selectionBox">
+				<div class="countButtonGroup" id="humanCountButtonGroup">
+					<input type="radio" name="humanCount" id="human2" , value="2">
+					<label for="human2" class="countButton">2</label>
+					<input type="radio" name="humanCount" id="human3" , value="3">
+					<label for="human3" class="countButton">3</label>
+					<input type="radio" name="humanCount" id="human4" , value="4">
+					<label for="human4" class="countButton">4</label>
+					<input type="radio" name="humanCount" id="human5" , value="5">
+					<label for="human5" class="countButton">5</label>
+					<input type="radio" name="humanCount" id="human6" , value="6">
+					<label for="human6" class="countButton">6</label>
+				</div>
+				<div class="countText">명</div>
+				<div id="vsText">VS</div>
+				<label class="selectionLabel" id="vampireLabel">뱀파이어</label>
+				<div class="countButtonGroup" id="vampireCounButtonGroup">
+					<input type="radio" name="vampireCountInput" id="vampire1", value="1" checked="checked">
+					<label for="vampire1" class="countButton">2</label>
+				</div>
+				<div class="countText">명</div>
 			</div>
-			<div class="countText">명</div>
-			<div id="vsText">VS</div>
-			<label class="selectionLabel" id="vampireLabel">뱀파이어</label>
-			<div class="countButtonGroup" id="vampireCounButtonGroup">
-				<input type="radio" name="vampireCountInput" id="vampire1", value="1" checked="checked">
-				<label for="vampire1" class="countButton">2</label>
-			</div>
-			<div class="countText">명</div>
 		`;
 	}
 
