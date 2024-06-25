63cd56f865a93cd748d017e1c1092e83412f8174
조영우 <66576231+choyoungwoo9@users.noreply.github.com>
2024-06-25 21:30:55 +0900

    tmp: 임시커밋

--- BE/pingpong/user/views.py
+++ BE/pingpong/user/views.py
@@ -1,63 +1,38 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-from .forms import SignUpForm
-from lobby.models import User
-from django.utils.decorators import async_only_middleware
 import json
 
-# @csrf_exempt
-
-@require_http_methods(["GET"])
-async def login(scope, receive, send):
-    # await asyncio.sleep(1)  # 비동기 작업 예시
-    await send 
-# def login(scope):
-#     async def asgi(receive, send):
-#         await send(
-#             {
-#                 "type": "http.response.start",
-#                 "status": 200,
-#                 "headers": [[b"content-type", b"text/plain"]],
-#             }
-#         )
-#         await send({"type": "http.response.body", "body": b"Hello, world!"})
-
-#     return asgi
-
+@csrf_exempt
 @require_http_methods(["POST"])
-async def signup(request):
+def signup(request):
     try:
-        # 비동기적으로 요청 본문 읽기
-        body = await request.body
-        body_data = body.decode('utf-8')
-        
-        # JSON 데이터 파싱
-        data = json.loads(body_data)
+        body = request.body.decode('utf-8')
+        data = json.loads(body)
     except json.JSONDecodeError:
         return JsonResponse({"error": "Invalid JSON"}, status=400)
-    # temp = await request.read()
-    # print("DD" , temp)
-    # print("ddd" , request.body.decode("utf-8"))
-    # data = json.loads(request.body.decode('utf-8'))
-    # except json.JSONDecodeError:
-    #     return JsonResponse({"error": "Invalid JSON"}, status=400)
 
-    # 데이터에서 사용자 정보 추출
-    # username = data.get('username')
-    # nickname = data.get('nickname')
-    # password = data.get('password')
-    # print(username)
-    # print(nickname)
-    # print(password)
-    # print(request.POST('username'))
-    # user = User.objects.create(username=request.POST['username'],
-    #                         password=request.POST['password'],
-    #                         email=request.POST['email'],)
-    # print(user)
-    # form = SignUpForm(request.POST)
-    # if form.is_valid():
-    #     user = form.save()
-    #     return JsonResponse({"user_id": user.id}, status=201)
-    # else:
-    return JsonResponse({"errors": "euijin"}, status=400)
+    # 여기서 data를 사용하여 추가 작업을 수행할 수 있습니다.
+    # 예를 들어, 회원가입 처리를 할 수 있습니다.
+
+    return JsonResponse({"message": "Signup successful"}, status=200)
+
+
+
+@require_http_methods(["POST"])
+def login(request):
+    # if request.method == 'POST':
+        # 요청 본문을 문자열로 디코딩
+    body_unicode = request.body.decode('utf-8')
+    # 문자열로 된 본문을 로그에 출력 (디버깅용)
+    print("Request body:", body_unicode)
+    
+    # # JSON 데이터를 파싱 (필요한 경우)
+    # body_data = json.loads(body_unicode)
+    
+    # 로그인 처리 로직
+    # 예: body_data에서 필요한 데이터를 사용하여 인증 로직 구현
+    return JsonResponse({"message": "Login successful"})
+    
+    # return JsonResponse({"error": "Invalid request method"}, status=400)
