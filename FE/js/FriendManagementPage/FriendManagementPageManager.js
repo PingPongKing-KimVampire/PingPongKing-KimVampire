eb05a09f93de8ad4588cbecfd4938e3b6b5a3b35
조영우 <66576231+choyoungwoo9@users.noreply.github.com>
2024-06-30 17:05:37 +0900

    feat: 친구요청 수락/거절, 친구 삭제 기능 구현

--- FE/js/FriendManagementPage/FriendManagementPageManager.js
+++ FE/js/FriendManagementPage/FriendManagementPageManager.js
@@ -1,4 +1,4 @@
-import windowObservable from "../../WindowObservable.js"
+import windowObservable from '../../WindowObservable.js';
 
 class FriendManagementPageManager {
 	constructor(app, clientInfo) {
@@ -6,20 +6,52 @@ class FriendManagementPageManager {
 
 		// TODO : 임시 하드 코딩
 		this.clientList = [
-			{ id: 3, nickname: "김뱀파이어어어어어어어어어어어어어", avatarUrl: "images/playerA.png" }, 
-			{ id: 3, nickname: "이뱀파이어", avatarUrl: "images/playerB.png" }, 
-			{ id: 3, nickname: "박뱀파이어", avatarUrl: "images/humanIcon.png" }, 
-			{ id: 3, nickname: "조뱀파이어", avatarUrl: "images/playerA.png" }, 
-			{ id: 3, nickname: "양뱀파이어", avatarUrl: "images/vampireIcon.png" }, 
-			{ id: 3, nickname: "박뱀파이어", avatarUrl: "images/playerB.png" }, 
-			{ id: 3, nickname: "박뱀파이어", avatarUrl: "images/humanIcon.png" }, 
-			{ id: 3, nickname: "박뱀파이어", avatarUrl: "images/playerB.png" }, 
-			{ id: 3, nickname: "조뱀파이어", avatarUrl: "images/playerA.png" }, 
-			{ id: 3, nickname: "양뱀파이어", avatarUrl: "images/vampireIcon.png" }
+			{
+				id: 3,
+				nickname: '김뱀파이어어어어어어어어어어어어어',
+				avatarUrl: 'images/playerA.png',
+			},
+			{ id: 3, nickname: '이뱀파이어', avatarUrl: 'images/playerB.png' },
+			{ id: 3, nickname: '박뱀파이어', avatarUrl: 'images/humanIcon.png' },
+			{ id: 3, nickname: '조뱀파이어', avatarUrl: 'images/playerA.png' },
+			{ id: 3, nickname: '양뱀파이어', avatarUrl: 'images/vampireIcon.png' },
+			{ id: 3, nickname: '박뱀파이어', avatarUrl: 'images/playerB.png' },
+			{ id: 3, nickname: '박뱀파이어', avatarUrl: 'images/humanIcon.png' },
+			{ id: 3, nickname: '박뱀파이어', avatarUrl: 'images/playerB.png' },
+			{ id: 3, nickname: '조뱀파이어', avatarUrl: 'images/playerA.png' },
+			{ id: 3, nickname: '양뱀파이어', avatarUrl: 'images/vampireIcon.png' },
 		];
+		this.clientInfo = {
+			friendInfo: {
+				friendList: [
+					{ id: 1, nickname: '조뱀파이어', avatarUrl: 'images/playerA.png' },
+					{ id: 2, nickname: '박뱀파이어', avatarUrl: 'images/humanIcon.png' },
+					{ id: 3, nickname: '이뱀파이어', avatarUrl: 'images/playerB.png' },
+					{ id: 4, nickname: '김뱀파이어', avatarUrl: 'images/playerA.png' },
+					{ id: 5, nickname: '최뱀파이어', avatarUrl: 'images/playerA.png' },
+					{ id: 6, nickname: '정뱀파이어', avatarUrl: 'images/playerA.png' },
+				],
+				clientListWhoFriendRequestedMe: [
+					{ id: 7, nickname: '이사람', avatarUrl: 'images/humanIcon.png' },
+					{ id: 8, nickname: '그사람', avatarUrl: 'images/humanIcon.png' },
+					{ id: 9, nickname: '저사람', avatarUrl: 'images/humanIcon.png' },
+					{ id: 10, nickname: '다른사람', avatarUrl: 'images/playerA.png' },
+					{ id: 11, nickname: '이웃사람', avatarUrl: 'images/playerB.png' },
+					{ id: 12, nickname: '친구사람', avatarUrl: 'images/playerA.png' },
+				],
+				clientListIFriendRequested: [
+					{ id: 13, nickname: '마지막사람', avatarUrl: 'images/playerK.png' },
+					{ id: 14, nickname: '새로운사람', avatarUrl: 'images/playerL.png' },
+					{ id: 15, nickname: '처음사람', avatarUrl: 'images/playerM.png' },
+					{ id: 16, nickname: '또다른사람', avatarUrl: 'images/playerN.png' },
+					{ id: 17, nickname: '모르는사람', avatarUrl: 'images/playerO.png' },
+					{ id: 18, nickname: '알고있는사람', avatarUrl: 'images/playerP.png' },
+				],
+			},
+		};
 
 		app.innerHTML = this._getHTML();
-		this.clientInfo = clientInfo;
+		// this.clientInfo = clientInfo;
 
 		this._initPage();
 	}
@@ -28,19 +60,175 @@ class FriendManagementPageManager {
 		this._setTabButtons();
 		this._autoSetScrollTrackColor();
 		this._subscribeWindow();
+		//모드에 따라 다른 notify 이벤트를 listen해 re render하는 메서드 구현
 	}
 
 	_setTabButtons() {
 		this.searchContainer = document.querySelector('#searchContainer');
 		this.userListContainer = document.querySelector('.userListContainer');
-		document.querySelector('#searchUserButton') // 유저 검색 탭
-				.addEventListener('click', this._setSearchContainer.bind(this, true));
-		document.querySelector('#friendRequestButton') // 친구 요청 목록 탭
-				.addEventListener('click', this._setSearchContainer.bind(this, false));
-		document.querySelector('#myFriendButton') // 내 친구 관리 탭
-				.addEventListener('click', this._setSearchContainer.bind(this, false));
-		document.querySelector('#blockUserButton') // 차단 유저 관리 탭
-				.addEventListener('click', this._setSearchContainer.bind(this, false));
+		document
+			.querySelector('#searchUserButton') // 유저 검색 탭
+			.addEventListener('click', this._setSearchContainer.bind(this, true));
+		document
+			.querySelector('#friendRequestButton') // 친구 요청 목록 탭
+			.addEventListener('click', () => {
+				this._setSearchContainer(false);
+				this._renderFriendRequestList();
+			});
+		document
+			.querySelector('#myFriendButton') // 내 친구 관리 탭
+			.addEventListener('click', () => {
+				this._setSearchContainer(false);
+				this._renderFriendList();
+			});
+		document
+			.querySelector('#blockUserButton') // 차단 유저 관리 탭
+			.addEventListener('click', this._setSearchContainer.bind(this, false));
+	}
+
+	_renderFriendRequestList() {
+		const innerContentContainer = document.querySelector(
+			'#innerContentContainer'
+		);
+		innerContentContainer.innerHTML = this._getFriendRequestListContainerHTML();
+		innerContentContainer
+			.querySelectorAll('.acceptButton')
+			.forEach((button) => {
+				button.addEventListener('click', (event) => {
+					const id = parseInt(event.target.closest('.userItem').dataset.id);
+					this._acceptFriendRequest(id);
+				});
+			});
+
+		innerContentContainer
+			.querySelectorAll('.rejectButton')
+			.forEach((button) => {
+				button.addEventListener('click', (event) => {
+					const id = parseInt(event.target.closest('.userItem').dataset.id);
+					this._rejectFriendRequest(id);
+				});
+			});
+	}
+
+	async _acceptFriendRequest(id) {
+		// const acceptFriendRequestMessage = {
+		// 	event: 'acceptFriendRequest',
+		// 	content: {
+		// 		clientInfo: {
+		// 			id,
+		// 		},
+		// 	},
+		// };
+		// this.clientInfo.socket.send(JSON.stringify(acceptFriendRequestMessage));
+		// await new Promise((resolve) => {
+		// 	const listener = (messageEvent) => {
+		// 		const { event, content } = JSON.parse(messageEvent.data);
+		// 		if (
+		// 			event === 'acceptFriendRequestResponse' &&
+		// 			content.message === 'OK'
+		// 		) {
+		// 			socket.removeEventListener('message', listener);
+		// 			resolve();
+		// 		}
+		// 	};
+		// });
+		const newFriendClient =
+			this.clientInfo.friendInfo.clientListWhoFriendRequestedMe.find(
+				(client) => client.id === id
+			);
+		if (newFriendClient) {
+			this.clientInfo.friendInfo.clientListWhoFriendRequestedMe =
+				this.clientInfo.friendInfo.clientListWhoFriendRequestedMe.filter(
+					(client) => client.id !== newFriendClient.id
+				);
+			this.clientInfo.friendInfo.friendList.push(newFriendClient);
+			this._renderFriendRequestList();
+		}
+	}
+
+	async _rejectFriendRequest(id) {
+		// const rejectFriendRequestMessage = {
+		// 	event: 'rejectFriendRequest',
+		// 	content: {
+		// 		clientInfo: {
+		// 			id,
+		// 		},
+		// 	},
+		// };
+		// this.clientInfo.socket.send(JSON.stringify(rejectFriendRequestMessage));
+		// this.clientInfo.socket.send(JSON.stringify(acceptFriendRequestMessage));
+		// await new Promise((resolve) => {
+		// 	const listener = (messageEvent) => {
+		// 		const { event, content } = JSON.parse(messageEvent.data);
+		// 		if (
+		// 			event === 'acceptFriendRequestResponse' &&
+		// 			content.message === 'OK'
+		// 		) {
+		// 			socket.removeEventListener('message', listener);
+		// 			resolve();
+		// 		}
+		// 	};
+		// });
+		const rejectedClient =
+			this.clientInfo.friendInfo.clientListWhoFriendRequestedMe.find(
+				(client) => client.id === id
+			);
+		if (rejectedClient) {
+			this.clientInfo.friendInfo.clientListWhoFriendRequestedMe =
+				this.clientInfo.friendInfo.clientListWhoFriendRequestedMe.filter(
+					(client) => client.id !== rejectedClient.id
+				);
+			this._renderFriendRequestList();
+		}
+	}
+
+	_renderFriendList() {
+		const innerContentContainer = document.querySelector(
+			'#innerContentContainer'
+		);
+		innerContentContainer.innerHTML = this._getFriendListContainerHTML();
+		innerContentContainer
+			.querySelectorAll('.deleteButton')
+			.forEach((button) => {
+				button.addEventListener('click', (event) => {
+					const id = parseInt(event.target.closest('.userItem').dataset.id);
+					this._deleteFriend(id);
+				});
+			});
+	}
+
+	async _deleteFriend(id) {
+		// const deleteFriendMessage = {
+		// 	event: 'deleteFriend',
+		// 	content: {
+		// 		clientInfo: {
+		// 			id,
+		// 		},
+		// 	},
+		// };
+		// this.clientInfo.socket.send(JSON.stringify(deleteFriendMessage));
+		// await new Promise((resolve) => {
+		// 	const listener = (messageEvent) => {
+		// 		const { event, content } = JSON.parse(messageEvent.data);
+		// 		if (
+		// 			event === 'deleteFriendResponse' &&
+		// 			content.message === 'OK'
+		// 		) {
+		// 			socket.removeEventListener('message', listener);
+		// 			resolve();
+		// 		}
+		// 	};
+		// });
+		const deletedClient = this.clientInfo.friendInfo.friendList.find(
+			(client) => client.id === id
+		);
+		if (deletedClient) {
+			this.clientInfo.friendInfo.friendList =
+				this.clientInfo.friendInfo.friendList.filter(
+					(client) => client.id !== deletedClient.id
+				);
+			this._renderFriendList();
+		}
 	}
 
 	_setSearchContainer(visible) {
@@ -59,16 +247,15 @@ class FriendManagementPageManager {
 		this._autoSetScrollTrackColorRef = this._autoSetScrollTrackColor.bind(this);
 		windowObservable.subscribeResize(this._autoSetScrollTrackColor);
 	}
-	_unsubscribeWindow() { // TODO : 페이지 나갈 시 호출
+	_unsubscribeWindow() {
+		// TODO : 페이지 나갈 시 호출
 		windowObservable.unsubscribeResize(this._autoSetScrollTrackColorRef);
 	}
 
-	_autoSetScrollTrackColor() { // TODO : 표시되는 리스트가 업데이트될 때도 호출해줘야 하지 않을까? 로비에서도?
+	_autoSetScrollTrackColor() {
+		// TODO : 표시되는 리스트가 업데이트될 때도 호출해줘야 하지 않을까? 로비에서도?
 		const userListContainier = document.querySelector('.userListContainer');
-		if (
-			userListContainier.scrollHeight > 
-			userListContainier.clientHeight
-		) {
+		if (userListContainier.scrollHeight > userListContainier.clientHeight) {
 			userListContainier.classList.add('transparent-scrolltrack');
 			userListContainier.classList.remove('scrollbar-scrolltrack');
 		} else {
@@ -111,8 +298,7 @@ class FriendManagementPageManager {
 	_getUserListContainerHTML() {
 		const userItemsHTML = this.clientList.reduce((acc, current) => {
 			return acc + this._getUserItemHTML(current);
-		}, "");
-		console.log(userItemsHTML);
+		}, '');
 		return `
 			<div class="userListContainer">
 				${userItemsHTML}
@@ -130,6 +316,63 @@ class FriendManagementPageManager {
 					<div class="userManagementButton friendRequestButtton activatedButton">친구 요청</div>
 					<div class="userManagementButton userBlockButtton activatedButton">차단</div>
 				</div>
+			</div>
+		`;
+	}
+
+	_getFriendRequestListContainerHTML() {
+		const getfriendRequestItemHtml = (client) => {
+			return `
+			<div class="userItem" data-id="${client.id}">
+				<div class="avatarImgFrame">
+					<img class="avatarImg" src="${client.avatarUrl}">
+				</div>
+				<div class="nickname">${client.nickname}</div>
+				<div class="buttonGroup">
+					<div class="userManagementButton acceptButton activatedButton">수락</div>
+					<div class="userManagementButton rejectButton activatedButton">거절</div>
+				</div>
+			</div>
+			`;
+		};
+		const clientRequestListHTML =
+			this.clientInfo.friendInfo.clientListWhoFriendRequestedMe.reduce(
+				(acc, current) => {
+					return acc + getfriendRequestItemHtml(current);
+				},
+				''
+			);
+		return `
+			<div class="userListContainer">
+				${clientRequestListHTML}
+			</div>
+		`;
+	}
+
+	_getFriendListContainerHTML() {
+		const getfriendItemHtml = (requestClient) => {
+			return `
+			<div class="userItem" data-id="${requestClient.id}">
+				<div class="avatarImgFrame">
+					<img class="avatarImg" src="${requestClient.avatarUrl}">
+				</div>
+				<div class="nickname">${requestClient.nickname}</div>
+				<div class="buttonGroup">
+					<div class="userManagementButton deleteButton activatedButton">친구 삭제</div>
+					<div class="userManagementButton blockButton activatedButton">차단</div>
+				</div>
+			</div>
+			`;
+		};
+		const clientListHTML = this.clientInfo.friendInfo.friendList.reduce(
+			(acc, current) => {
+				return acc + getfriendItemHtml(current);
+			},
+			''
+		);
+		return `
+			<div class="userListContainer">
+				${clientListHTML}
 			</div>
 		`;
 	}
