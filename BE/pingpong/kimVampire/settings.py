1adebd9158af32f01e3beb2ba5c2ace7f0ebee98
blackhyeon <nij9676@gmail.com>
2024-06-30 18:50:55 +0900

    feat: image upload

--- BE/pingpong/kimVampire/settings.py
+++ BE/pingpong/kimVampire/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = 'BE:data_image/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'data_image')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
