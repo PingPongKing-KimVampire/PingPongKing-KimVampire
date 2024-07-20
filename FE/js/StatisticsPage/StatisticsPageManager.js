40504c27194bb0f2a8ba55cab2e08f6c0134be0a
RoWoon Lee <lru040409@gmail.com>
2024-07-20 16:31:41 +0900

    feat: 라운드 별 점수 그래프 디자인 구현 완료

--- FE/js/StatisticsPage/StatisticsPageManager.js
+++ FE/js/StatisticsPage/StatisticsPageManager.js
@@ -1,13 +1,72 @@
+import windowObservable from "../../WindowObservable.js";
+
 class StatisticsPageManager {
 	constructor(app, clientInfo) {
 		this.app = app;
 		this.clientInfo = clientInfo;
 
+		this.points1 = [1, 2, 2, 2, 2, 3, 3, 3]; // TODO : 임시 하드코딩
+		this.points2 = [0, 0, 1, 2, 3, 3, 4, 5];
+		this.maxPoint = Math.max(...this.points1, ...this.points2);
+		this.round = 8;
+
 		this._initPage();
 	}
-	
+
 	_initPage() {
 		this.app.innerHTML = this._getHTML();
+		this._subscribeWindow();
+		requestAnimationFrame(this._renderScoreGraph.bind(this));
+	}
+
+	_subscribeWindow() {
+		this._renderScoreGraphRef = this._renderScoreGraph.bind(this);
+		windowObservable.subscribeResize(this._renderScoreGraphRef);
+	}
+	_unsubscribeWindow() { // TODO : 화면 나갈 때 호출하기
+		windowObservable.unsubscribeResize(this._renderScoreGraphRef);
+	}
+
+	_renderScoreGraph() {
+		const lineCanvas = document.querySelector('#graphLineCanvas');
+		lineCanvas.innerHTML = '';
+		const circleCanvas = document.querySelector('#graphCircleCanvas');
+		circleCanvas.innerHTML = '';
+		const graphRect = document.querySelector('#graphContainer').getBoundingClientRect();
+		const scale = { x: graphRect.width / this.round, y: graphRect.height / this.maxPoint };
+
+		const renderLine = (points, color) => {
+			function createLine(pos1, pos2, color) {
+				const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+				line.setAttribute('x1', pos1.x);
+				line.setAttribute('y1', pos1.y);
+				line.setAttribute('x2', pos2.x);
+				line.setAttribute('y2', pos2.y);
+				line.setAttribute('stroke', color);
+				line.setAttribute('stroke-width', '0.3rem');
+				lineCanvas.append(line);
+			}
+			function createCircle(pos) {
+				const circleButton = document.createElement('button');
+				circleButton.classList.add('circleButton');
+				circleButton.style.left = `${pos.x}px`;
+				circleButton.style.top = `${pos.y}px`;
+				circleCanvas.append(circleButton);
+			}
+			let prevPoint = { x: graphRect.left, y: graphRect.bottom }
+			points.forEach((point, index) => {
+				const currentPoint = {
+					x: graphRect.left + ((index + 1) * scale.x),
+					y: graphRect.bottom - (point * scale.y)
+				}
+				createLine(prevPoint, currentPoint, color);
+				createCircle(currentPoint);
+				prevPoint = currentPoint;
+			});
+		}
+
+		renderLine(this.points1, '#BEBEBE');
+		renderLine(this.points2, '#D570FF');
 	}
 
 	_getHTML() {
@@ -17,6 +76,8 @@ class StatisticsPageManager {
 				${this._getMainInfoContainerHTML()}
 				${this._getSubInfoContainerHTML()}
 			</div>
+			<svg id="graphLineCanvas"></svg>
+			<div id="graphCircleCanvas"></div>
 		`;
 	}
 	_getMainInfoContainerHTML() {
@@ -56,8 +117,24 @@ class StatisticsPageManager {
 	}
 	_getScorePanelHTML() {
 		return `
-			<div id="scorePanel"></div>
+			<div id="scorePanel">
+				<div id="graphContainer">
+					<div id="xLabels">
+						${this._getGraphLabelsHTML(0, this.round)}
+						</div>
+					<div id="yLabels">
+						${this._getGraphLabelsHTML(0, this.maxPoint)}
+					</div>
+				</div>
+			</div>
 		`;
+	}
+	_getGraphLabelsHTML(start, end) {
+		let labels = '';
+		for (let count = start; count <= end; count++) {
+			labels += `<div>${count}</div>`;
+		}
+		return labels;
 	}
 	_getHitMapPanelHTML() {
 		return `
