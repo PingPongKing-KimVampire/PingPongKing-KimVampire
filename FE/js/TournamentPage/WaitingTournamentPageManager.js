54cbdad831c621efe5dd2537a21cccba92002b18
RoWoon Lee <lru040409@gmail.com>
2024-08-10 12:59:33 +0900

    feat: 토너먼트 대기실 페이지 탐색큐 이미지, 텍스트 반응형 고려 완료

--- FE/js/TournamentPage/WaitingTournamentPageManager.js
+++ FE/js/TournamentPage/WaitingTournamentPageManager.js
@@ -73,29 +73,6 @@ class WaitingTournamentPageManager {
 		});
 	}
 
-	// _subscribeWindow() {
-	// 	this._adjustButtonSizeRef = this._adjustButtonSize.bind(this);
-	// 	windowObservable.subscribeResize(_adjustButtonSizeRef);
-	// }
-
-	// _unsubscribeWindow() {
-	// 	windowObservable.unsubscribeResize(this._adjustButtonSizeRef);
-	// }
-
-	// _adjustButtonSize() {
-	// 	const leaveWaitingTournamentButton = document.querySelector(".leaveWaitingTournamentButton");
-	// 	const viewWidth = window.innerWidth;
-	// 	const viewHeight = window.innerHeight;
-
-	// 	if (viewWidth < viewHeight) {
-	// 		leaveWaitingTournamentButton.style.height = "4vh";
-	// 		leaveWaitingTournamentButton.style.width = "calc(4vh * 5 / 1)";
-	// 	} else {
-	// 		leaveWaitingTournamentButton.style.width = "20vw";
-	// 		leaveWaitingTournamentButton.style.height = "calc(20vw * 1 / 4)";
-	// 	}
-	// }
-
 	_getHTML() {
 		return `
         <div class="waitingTournament">
