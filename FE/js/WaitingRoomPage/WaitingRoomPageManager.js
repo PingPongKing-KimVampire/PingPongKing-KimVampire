5c105ec52fe64c6276243c451f06c9704ef6c4d5
RoWoon Lee <lru040409@gmail.com>
2024-08-10 11:44:59 +0900

    feat: 대기실 페이지 반응형 고려 완료

--- FE/js/WaitingRoomPage/WaitingRoomPageManager.js
+++ FE/js/WaitingRoomPage/WaitingRoomPageManager.js
@@ -81,20 +81,11 @@ class WaitingRoomPageManager {
 
 	initPage() {
 		this._setMeInfo();
-		this.app.innerHTML = this._getHTML();
-
-		this.leftReadyText = document.querySelector(".teamPanel:first-of-type .readyText");
-		this.rightReadyText = document.querySelector(".teamPanel:last-of-type .readyText");
-		this._subscribeWindow();
-		document.querySelector("#readyButton").addEventListener("click", event => {
-			this._sendMyReadyStateChangeMessage.call(this);
-		});
-		this._manageExitRoom();
-		this._setAbilityButtons();
+		this._renderPage();
 		this._listenWaitingRoomEvent();
 	}
 
-	_rerenderPage() {
+	_renderPage() {
 		this.app.innerHTML = this._getHTML();
 		this.leftReadyText = document.querySelector(".teamPanel:first-of-type .readyText");
 		this.rightReadyText = document.querySelector(".teamPanel:last-of-type .readyText");
@@ -151,16 +142,16 @@ class WaitingRoomPageManager {
 			if (event === "notifyWaitingRoomEnter") {
 				const { id, nickname, team, avatarUrl } = content;
 				this._pushNewPlayer(id, nickname, team, avatarUrl);
-				this._rerenderPage();
+				this._renderPage();
 			} else if (event === "notifyWaitingRoomExit") {
 				const clientId = content.clientId;
 				this._popPlayer(clientId);
-				this._rerenderPage();
+				this._renderPage();
 			} else if (event === "notifyReadyStateChange") {
 				const { clientId, state } = content;
 				this._updateReadyState(clientId, state);
 				if (clientId === this.clientInfo.id) this.me.readyState = state;
-				this._rerenderPage();
+				this._renderPage();
 			} else if (event === "notifyGameReady") {
 				//3, 2, 1 추후 구현
 			} else if (event === "notifyGameStart") {
@@ -184,7 +175,7 @@ class WaitingRoomPageManager {
 				if (content.team === this.me.team && this.myAbilityButton) {
 					this.me.selectAbility = true;
 				}
-				this._rerenderPage();
+				this._renderPage();
 			}
 		};
 		this.clientInfo.gameInfo.pingpongRoomSocket.addEventListener("message", this.listener);
@@ -251,6 +242,7 @@ class WaitingRoomPageManager {
 	}
 
 	_toggleReadyTextVisible(orientation) {
+		console.log(orientation);
 		if (orientation === "landscape") {
 			this.leftReadyText.classList.remove("invisible");
 			this.rightReadyText.classList.remove("invisible");
