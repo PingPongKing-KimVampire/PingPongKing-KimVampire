1d715806a136469e8937ab61e801c842de6c7592
Seojun Yang <117631902+yseojun@users.noreply.github.com>
2024-08-11 20:55:47 +0900

    feat: 공이 나름의 물리법칙을 따르도록 계산 추가

--- BE/pingpong/pingpongRoom/gameManage/ball.py
+++ BE/pingpong/pingpongRoom/gameManage/ball.py
@@ -7,6 +7,8 @@ NORMAL_ANGLE = 40
 SPEEDTWIST_ANGLE = 70
 SPEEDTWIST_SPEED_RATIO = 1.5
 
+ANGLE_CORRECTION = 20
+
 class Ball:
     def __init__(self, speed=NORMAL_SPEED, radius=25, hit_count = 0):
         self.speed = speed
@@ -51,6 +53,12 @@ class Ball:
         self.dx = dir['dx']
         self.dy = dir['dy']
 
+    def get_angle(self):
+        movement_influence = math.atan2(self.dy, self.dx)
+        movement_angle = math.degrees(movement_influence)
+        movement_angle = (movement_angle + 270) % 360
+        return movement_angle
+
     def get_right_x(self):
         return self.pos_x + self.radius
 
@@ -70,20 +78,66 @@ class Ball:
             serve_position = 3 * board_width / 4
         self.reset_ball(serve_position, board_height / 2)
         self.pause()
+
+    def correction_angle(self, angle):
+        if 360 - ANGLE_CORRECTION < angle < 360:
+            return 360 - ANGLE_CORRECTION
+        elif 0 < angle < ANGLE_CORRECTION:
+            return ANGLE_CORRECTION
+        elif 180 < angle < 180 + ANGLE_CORRECTION:
+            return 180 + ANGLE_CORRECTION
+        elif 180 - ANGLE_CORRECTION < angle < 180:
+            return 180 - ANGLE_CORRECTION
+        elif angle in (0, 180):
+            return None
+        return angle
+    
+    def apply_collision(self, paddle_angle):
+        ball_angle = self.get_angle()
         
-    def set_ball_speed_twist(self):
-        self.speed = self.speed * 2
+        if not (0 <= ball_angle < 360):
+            return None
         
-    def reversal_by_player(self, movement_angle):
+        if not (0 <= paddle_angle < 360):
+            return None
+        
+        if 0 <= ball_angle < 180:
+            reflected_angle = 360 - ball_angle
+        else:  # 180 <= ball_angle < 360
+            reflected_angle = 360 - ball_angle
+        
+        if 0 <= paddle_angle < 180:
+            adjustment = (paddle_angle - 90) / 2
+        else:  # 180 <= paddle_angle < 360
+            adjustment = (paddle_angle - 270) / 2
+        
+        final_angle = (reflected_angle + adjustment) % 360
+        
+        return final_angle
+
+    def reversal_by_player(self, paddle_angle):
+        if not self.speed == 0:
+            angle = self.apply_collision(paddle_angle)
+        else:
+            angle = paddle_angle
+        if angle == None:
+            return
+        angle = self.correction_angle(angle)
+        if angle == None:
+            return
+
+        angle = (angle - 270) % 360
+        if angle > 180:
+            angle -= 360
+        
         if not self.is_speedtwist:
             self.speed = NORMAL_SPEED + self.hit_count
-            self.angle = movement_angle
+            self.angle = angle
         else: 
             self.speed = (NORMAL_SPEED + self.hit_count) * SPEEDTWIST_SPEED_RATIO
             rand = random.randint(-SPEEDTWIST_ANGLE, SPEEDTWIST_ANGLE)
             self.angle = rand
         
-        # 새로운 방향 계산 및 설정
         dir = self._calculate_ball_direction()
         self.dx = dir['dx']
         self.dy = dir['dy']
