be1239e9a7e4d0c1718054b69dad081b5142a796
RoWoon Lee <lru040409@gmail.com>
2024-08-11 12:49:52 +0900

    feat: 게임 통계 페이지 반응형 고려 완료

--- FE/js/PageRouter.js
+++ FE/js/PageRouter.js
@@ -184,7 +184,7 @@ class PageRouter {
 				this._inVisibleChatButton();
 				this.nextPageManager = new ErrorPageManager(this.app, this.clientInfo, this.renderPage.bind(this));
 			} else if (url === "statistics") {
-				this._loadCSS(["css/StatisticsPage/statisticsPage.css", "css/ProfilePage/matchLog.css"]);
+				this._loadCSS(["css/ProfilePage/matchLog.css", "css/StatisticsPage/statisticsPage.css"]);
 				this._visibleChatButton();
 				this.nextPageManager = new StatisticsPageManager(this.app, this.clientInfo, this.renderPage.bind(this), queryParam);
 			}
